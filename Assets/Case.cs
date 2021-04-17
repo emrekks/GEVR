b68f757adb0ebd49e21f0ff8578aa999acb076d7
Emre <emrekks32@gmail.com>
2021-04-17 14:25:28 +0300

    abc

---
+++ Assets/Case.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Case : MonoBehaviour
+{
+    [SerializeField] private int MoneyAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            if (collision.gameObject.CompareTag("Money1"))
+            {
+                MoneyAmount += 1;
+            }
+            if (collision.gameObject.CompareTag("Money5"))
+            {
+                MoneyAmount += 5;
+            }
+            if (collision.gameObject.CompareTag("Money10"))
+            {
+                MoneyAmount += 10;
+            }
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
