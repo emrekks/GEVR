a9a27757f2dfe51cc2d32a77d6a20c14408b3b0b
Emre <emrekks32@gmail.com>
2021-05-25 01:30:41 +0300

    LastV

--- Assets/Scripts/PurpleTicketButton.cs
+++ Assets/Scripts/PurpleTicketButton.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PurpleTicketButton : MonoBehaviour
 {
+    public GameObject PurplePrefab;
+    public GameObject PurpleButton;
+    private Vector3 PrefabRef;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrefabRef = new Vector3(PurpleButton.gameObject.transform.position.x, PurpleButton.gameObject.transform.position.y, PurpleButton.gameObject.transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Hand")
+        {
+            Instantiate(PurplePrefab, PrefabRef, Quaternion.identity);
+        }
     }
 }
